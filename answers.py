7edeae9f5bcd8c31d12b23fd158fc469a3bd0b95
lunn06 <81339721+lunn06@users.noreply.github.com>
2022-12-14 02:56:45 +0700

    Add files via upload

---
+++ answers.py
@@ -0,0 +1,41 @@
+def answers_from(answers):
+    ANS_COUNT = 11
+
+    data = [0]*ANS_COUNT
+    for i, ans in enumerate(answers):
+        if ans.lower() == "да":
+            data[(i%ANS_COUNT)-1] += 1
+    
+    skills = [
+            "Cпособность управлять собой",
+            "Чёткие ценности",
+            "Чёткие личные цели", "Продолжающееся саморазвитие",
+            "Хорошие навыки решения проблем",
+            "Творческий подход",
+            "Умение влиять на окружающих",
+            "Понимание особенностей управленческого труда",
+            "Способность руководить",
+            "Умение обучать",
+            "Cпособность управлять собой",
+    ]
+
+    indexs = list(range(ANS_COUNT))
+    for i in range(1, ANS_COUNT):
+        c = 0
+        j = i
+        while j > 0 and data[j-1] > data[j]:
+            c += 1
+            tmp = data[j]
+            data[j] = data[j-1]
+            data[j-1] = tmp
+            j -= 1
+        indexs.pop(i)
+        indexs.insert(i-c,  i)
+
+    return {
+            "Сильные стороны": [skills[i] for i in indexs[-3:]],
+            "Слабые стороны": [skills[i] for i in indexs[:3]]
+    }
+
+if __name__ == "__main__":
+    print(answers_from(['да']))
